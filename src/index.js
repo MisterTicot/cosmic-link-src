05df5b39f7de2b70ea2de16903d89ddb986e07e3
MisterTicot <mister.ticot@cosmic.plus>
2018-12-04 16:54:40 -0500

    index.js: First register service worker

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@
  * Entry point of Cosmic.Link application. We redirect without showing the
  * interface when possible. Else, we load the GUI.
  */
-require("@cosmic-plus/jsutils/polyfill")
-const authenticators = require("./authenticators")
-const the = require("./shared")
 
 // Service worker
 if (navigator.serviceWorker) navigator.serviceWorker.register("worker.js")
+
+// Requires
+require("@cosmic-plus/jsutils/polyfill")
+const authenticators = require("./authenticators")
+const the = require("./shared")
 
 // Variables initialization
 the.query = location.search.length > 1 && location.search
