92c71c36307186f926db8c4289f614dfbb99c8cc
MisterTicot <mister.ticot@cosmic.plus>
2019-02-20 11:44:31 +0400

    Tweak vendors configuration & improve app caching

--- src/worker.js
+++ src/worker.js
@@ -5,6 +5,7 @@ const pkg = require("../package.json")
 
 new ServiceWorker(pkg.name, pkg.version, "verbose")
   .fromCache([
+    // Application
     "/",
     "app.js",
     "index.css",
@@ -12,9 +13,21 @@ new ServiceWorker(pkg.name, pkg.version, "verbose")
     "index.js",
     "ledger.js",
     "stellar-sdk.js",
-    "icons/512x512.png",
     "vendors~app.js",
-    "vendors~ledger.js"
+    "vendors~ledger.js",
+
+    // Vendor configuration
+    "browserconfig.xml",
+    "manifest.json",
+
+    // Icons
+    "icons/16x16.png",
+    "icons/32x32.png",
+    "icons/192x192.png",
+    "icons/512x512.png",
+    "icons/apple-touch.png",
+    "icons/mstile.png",
+    "icons/safari.svg"
   ])
   .precache()
   .register()
