2b965f6b9da762f89b1994870cadf563585163a2
MisterTicot <mister.ticot@cosmic.plus>
2019-11-23 18:51:54 +0700

    logic Fix: prevent loading of unused resources
    Some GUI-related resources were loaded even when immediately redirecting
    to wallet.

--- src/index.js
+++ src/index.js
@@ -4,43 +4,59 @@
  * interface when possible. Else, we load the GUI.
  */
 
-// Service worker
-const worker = navigator.serviceWorker
-if (worker) {
-  worker.register("worker.js")
-  worker.addEventListener("controllerchange", () => location.reload())
-}
-
 // Requires
 require("@cosmic-plus/domutils/es5/polyfill")
 const authenticators = require("./authenticators")
 const the = require("./shared")
 
-// Context awarness
-const pageName = location.pathname.replace(/.*\//, "")
-the.contextIsWidget = !!pageName.match(/^widget(.html)?$/)
+function init () {
+  // Context awareness
+  const pageName = location.pathname.replace(/.*\//, "")
+  the.contextIsWidget = !!pageName.match(/^widget(.html)?$/)
 
-// Variables initialization
-the.query = location.search.length > 1 && location.search
-the.authenticator =
-  authenticators[localStorage.authenticator]
-  || authenticators["Stellar Authenticator"]
-the.redirect = !the.contextIsWidget && localStorage.redirect === "true"
-the.qrCode = localStorage.QR === "true"
+  // Variables initialization
+  the.query = location.search.length > 1 && location.search
+  the.authenticator =
+    authenticators[localStorage.authenticator]
+    || authenticators["Stellar Authenticator"]
+  the.redirect = !the.contextIsWidget && localStorage.redirect === "true"
+  the.qrCode = localStorage.QR === "true"
 
-// Immediate redirection when possible
-if (the.query && the.redirect && the.authenticator.protocol === "cosmiclink") {
-  const target = the.authenticator.url + location.search
-  location.replace(target)
-} else if (pageName === "index.html") {
-  history.replaceState(
-    {},
-    "",
-    location.pathname.replace(/index.html$/, "")
-      + location.search
-      + location.hash
-  )
+  if (
+    the.query
+    && the.redirect
+    && the.authenticator.protocol === "cosmiclink"
+  ) {
+    redirect()
+  } else {
+    loadInterface(pageName)
+  }
 }
 
-// GUI loading
-window.onload = () => import(/* webpackChunkName: "app" */ "./app")
+function redirect () {
+  const target = the.authenticator.url + location.search
+  location.replace(target)
+}
+
+function loadInterface (pageName) {
+  // Service worker (does it means users with automatic redirection set never
+  // get updates?)
+  const worker = navigator.serviceWorker
+  if (worker) {
+    worker.register("worker.js")
+    worker.addEventListener("controllerchange", () => location.reload())
+  }
+
+  // (hack) api.cosmic.link redirects at `index.html` to shortcut itself, but we
+  // don't want it to show into the address bar.
+  if (pageName === "index.html") {
+    const path = location.pathname.replace(/index.html$/, "")
+    const url = `${path}${location.search}${location.hash}`
+    history.replaceState({}, "", url)
+  }
+
+  // GUI loading
+  window.onload = () => import(/* webpackChunkName: "app" */ "./app")
+}
+
+init()
