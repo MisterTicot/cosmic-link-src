MisterTicot/cosmic-link-src

commit f31814d78398f37567471f6ec56af2454ea6d579

dev Change: replace @cosmic-plus/domutils by local library

src/lib/page.js
@@ -1,0 +1,86 @@
+"use strict"
+/**
+ * Web application pages that are embeded into URL hash.
+ *
+ * @exports Page
+ */
+const { dom, html } = require("@kisbox/browser")
+
+const main = html.grab("main")
+
+/* Definition */
+
+class Page {
+  static resolve (hash) {
+    return Page.table[hash]
+  }
+
+  static add (title, domNode) {
+    const page = new Page(title, domNode)
+    Page.table[page.hash] = page
+
+    html.append(main, page.contentNode)
+    if (dom.nav) html.append(dom.nav, page.linkNode)
+
+    if (location.hash === page.hash) page.select()
+    else html.hide(page.contentNode)
+
+    return page
+  }
+
+  static select (hash) {
+    const page = Page.resolve(hash)
+    if (page) page.select()
+    else throw new Error(`Can't find page: ${hash}`)
+  }
+
+  static list () {
+    return Object.values(Page.table)
+  }
+
+  constructor (title, domNode) {
+    this.title = title
+    if (!domNode.id) domNode.id = title.toLowerCase()
+    this.hash = `#${domNode.id}`
+
+    if (!domNode.parentNode) dom.ingest(domNode)
+    domNode.classList.add("page")
+    this.contentNode = domNode
+
+    this.linkNode = html("a", [title])
+    this.linkNode.onclick = () => this.select()
+  }
+
+  get isSelected () {
+    return Page.current === this
+  }
+
+  select () {
+    if (this.isSelected) return
+    else if (Page.current) Page.current.unselect()
+
+    history.replaceState("", null, location.search + this.hash)
+    html.show(this.contentNode)
+    this.linkNode.className = "selected"
+    this.linkNode.onclick = undefined
+    Page.current = this
+
+    if (this.onSelect) this.onSelect(this)
+  }
+
+  unselect () {
+    if (Page.current === this) {
+      history.replaceState("", null, location.search)
+      html.hide(this.contentNode)
+      this.linkNode.className = null
+      this.linkNode.onclick = () => this.select()
+      Page.current = undefined
+    }
+  }
+}
+
+Page.table = {}
+Page.current = undefined
+
+/* Exports */
+module.exports = Page

src/app.interface.js
@@ -5,7 +5,7 @@
 const cosmicLib = require("cosmic-lib")
 
 const { dom, html } = require("@kisbox/browser")
-const Page = require("@cosmic-plus/domutils/es5/page")
+const Page = require("./lib/page")
 
 const the = require("./app.state")
 const { copyContent } = require("./helpers")

commit 87aee36e767074f97f3a77203a126f014baf02a1

dev Fix a navigation regression
(introduced while refactoring in commit f31814d)

src/lib/page.js
@@ -5,8 +5,7 @@
  * @exports Page
  */
 const { dom, html } = require("@kisbox/browser")
-
-const main = html.grab("main")
+dom.main = html.grab("main")
 
 /* Definition */
 
@@ -19,7 +18,7 @@ class Page {
     const page = new Page(title, domNode)
     Page.table[page.hash] = page
 
-    html.append(main, page.contentNode)
+    html.append(dom.main, page.contentNode)
     if (dom.nav) html.append(dom.nav, page.linkNode)
 
     if (location.hash === page.hash) page.select()

static/index.html
@@ -60,7 +60,7 @@
   <img id="logo" src="icons/512x512.png" alt="">
 
   <main>
-    <nav> </nav>
+    <nav id="nav"> </nav>
     <hr>
 
     <section id="sign">