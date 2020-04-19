1cbba10786d1b02e459f2e695722cb2ca50068dd
MisterTicot <mister.ticot@cosmic.plus>
2020-04-19 23:24:48 +0200

    dev Add 'data/wallets'

---
+++ src/data/wallets.js
@@ -0,0 +1,65 @@
+"use strict"
+/**
+ * Wallets & signing methods that accept arbitrary transactions as input.
+ */
+const wallets = exports
+
+/* Data */
+wallets.LedgerWallet = {
+  name: "Ledger Wallet",
+  protocol: "ledgerwallet",
+  needSource: true,
+  needNetwork: true
+}
+
+wallets.TrezorWallet = {
+  name: "Trezor Wallet",
+  protocol: "trezorwallet",
+  needSource: true,
+  needNetwork: true
+}
+
+wallets.StellarAuthenticator = {
+  name: "Stellar Authenticator",
+  protocol: "cosmiclink",
+  url: "https://stellar-authenticator.org/"
+}
+
+wallets.Lobstr = {
+  name: "Lobstr Web (partial support)",
+  protocol: "sep0007",
+  url: "https://lobstr.co/?tx=",
+  target: "new"
+}
+
+wallets.StellarTerm = {
+  name: "StellarTerm (partial support)",
+  protocol: "sep0007",
+  url: "https://stellarterm.com/?tx=",
+  target: "new"
+}
+
+wallets.Sep7Wallet = {
+  name: "Sep-0007 Wallet",
+  protocol: "sep0007",
+  url: "web+stellar:",
+  target: "external",
+  needSource: true,
+  needNetwork: true
+}
+
+wallets.StellarLaboratory = {
+  name: "Stellar Laboratory",
+  protocol: "stellarlab",
+  url: "https://stellar.org/laboratory/#txsigner",
+  target: "new",
+  needSource: true,
+  needNetwork: true
+}
+
+wallets.CopyPasteXdr = {
+  name: "Copy/Paste XDR",
+  protocol: "copy",
+  needSource: true,
+  needNetwork: true
+}
