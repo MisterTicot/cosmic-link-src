62f88a3ffe5b5546b58a04bf6cc408f9c3781c6e
MisterTicot <mister.ticot@cosmic.plus>
2020-04-19 23:10:05 +0200

    dev Add 'app.embed.js'

---
+++ src/app.embed.js
@@ -0,0 +1,30 @@
+"use strict"
+/**
+ * Widget interface initialization.
+ */
+const { View } = require("@kisbox/browser")
+
+const AppState = require("./app.state")
+const SigningWidget = require("./view/signing-widget")
+
+/* Definition */
+
+class AppEmbed extends View {
+  constructor (params) {
+    super(`
+<div class="CosmicLinkApp" %hidden>
+  <main>
+    <hr>
+    %signingWidget
+  </main>
+</div>
+    `)
+
+    this.state = new AppState(params)
+    this.signingWidget = new SigningWidget(this.state)
+    this.$pull("route", this.state, "query")
+  }
+}
+
+/* Export */
+module.exports = AppEmbed
